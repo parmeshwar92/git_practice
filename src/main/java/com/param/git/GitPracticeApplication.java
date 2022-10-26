01f95097c872c9d90badeb9a249d05ad0a19bd5e
parmeshwar <parmeshwar92@gmail.com>
2022-10-26 16:50:31 +0530

    first commit added logger

--- src/main/java/com/param/git/GitPracticeApplication.java
+++ src/main/java/com/param/git/GitPracticeApplication.java
@@ -1,13 +1,17 @@
 package com.param.git;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitPracticeApplication {
+	private static final Logger logger= LoggerFactory.getLogger(GitPracticeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitPracticeApplication.class, args);
+		logger.info("first commit");
 	}
 
 }
