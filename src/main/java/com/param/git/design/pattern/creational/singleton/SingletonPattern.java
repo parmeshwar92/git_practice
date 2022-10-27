0c7b78370f7b2ec7eb2b095bb35c8239c409e4ef
parmeshwar <parmeshwar92@gmail.com>
2022-10-27 16:36:54 +0530

    working on design pattern

--- src/main/java/com/param/git/design/pattern/creational/singleton/SingletonPattern.java
+++ src/main/java/com/param/git/design/pattern/creational/singleton/SingletonPattern.java
@@ -2,4 +2,22 @@ package com.param.git.design.pattern.creational.singleton;
 
 public class SingletonPattern {
 
+	private static SingletonPattern obj;
+
+	private SingletonPattern(){}
+
+	public static SingletonPattern getSingletonPattern() {
+		if (obj == null) {
+			synchronized (SingletonPattern.class) {
+				if (obj == null) {
+					obj = new SingletonPattern();// instance will be created at request time
+				}
+			}
+		}
+		return obj;
+	}
+
+	public void doSomething() {
+		// write your code
+	}
 }
